410c4dba12e9392922188afee96c732afd9924aa
chenji <chenji@letv.com>
2016-05-10 21:25:01 -0400

    updata cipostbuild Signed-off-by: chenji <chenji@letv.com>
    Change-Id: I3b9b073f9e17bb756c4212cba9cb0e91aeaf8bb4

--- ci/cipostbuild.sh
+++ ci/cipostbuild.sh
@@ -2,12 +2,14 @@
 filepath=$(cd "$(dirname "$0")"; pwd)
 cd $filepath
 
-repo='10.200.93.170'
+repo='10.140.75.88'
 remotedir="/letv/yum-repo/ceph/el6/update"
 sshpasspath=`which sshpass`
 if [[ $? -ne  0 ]] ;then
         echo "try to install sshpass first"
         exit
 fi
-$sshpasspath -p Myiaas.chensh.net scp -o StrictHostKeyChecking=no $(find .. -type f -name "*.rpm" -a ! -name "*src*") chensh@$repo:$remotedir/x86_64
-$sshpasspath -p Myiaas.chensh.net ssh chensh@$repo createrepo --update $remotedir
+$sshpasspath -p TVLEhp800g.com scp -o StrictHostKeyChecking=no $(find .. -type f -name "*.rpm" -a ! -name "*src*") root@$repo:$remotedir/x86_64
+$sshpasspath -p TVLEhp800g.com ssh root@$repo createrepo --update $remotedir
+$sshpasspath -p TVLEhp800g.com ssh root@$repo /letv/yum-repo/sync.ceph
+rm ~/rpmbuild/RPMS/x86_64/*.rpm -rf

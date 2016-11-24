fdd8b93a50c74199d110544e37f665102fe5335d
Dongmao Zhang <deanraccoon@gmail.com>
2016-11-24 17:53:21 +0800

    add rados truncate API
    Change-Id: I416b6b035596d4ad68855ec7533710ca05e7e4c6

--- rados/striper.go
+++ rados/striper.go
@@ -53,6 +53,16 @@ func (sp *StriperPool) State(oid string) (uint64,uint64, error) {
     return uint64(c_psize), uint64(c_ptime), nil
 }
 
+func (sp *StriperPool) Truncate(oid string, uint64 offset) error{
+    c_oid := C.CString(oid)
+    defer C.free(unsafe.Pointer(c_oid))
+	ret := C.rados_striper_truncate(sp.striper, c_oid, C.uint64(offset))
+	if ret < 0 {
+		return RadosError(int(ret))
+	}
+	return nil
+}
+
 func (sp *StriperPool) Delete(oid string) error {
     c_oid := C.CString(oid)
     defer C.free(unsafe.Pointer(c_oid))

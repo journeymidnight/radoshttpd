c516d24a09d9b8e893a1528e84bde9f3d44cc315
caolix <695629989@qq.com>
2021-08-25 15:19:05 +0800

    Add cephdf and EC profile info

--- rados/conn.go
+++ rados/conn.go
@@ -272,3 +272,45 @@ func (c *Conn) Status() (string, error) {
         return "", RadosError(ret)
     }
 }
+
+func (c *Conn) GetCephDf() (string, error) {
+    c_cmd := C.CString("{\"prefix\":\"df\", \"format\":\"json-pretty\"}")
+    var c_buf *C.char = nil
+    var c_buf_len C.size_t = 0
+    ret := C.rados_mon_command(c.cluster, &c_cmd, 1, nil, 0, &c_buf, &c_buf_len, nil, nil)
+    defer C.free(unsafe.Pointer(c_buf))
+    if ret == 0 {
+        return C.GoStringN(c_buf, C.int(c_buf_len)), nil
+    } else {
+        return "", RadosError(ret)
+    }
+}
+
+func (c *Conn) GetECProfileInfo(profile string) (string, error) {
+    c_cmd := C.CString("{\"prefix\":\"osd erasure-code-profile get\", \"name\": \"" + profile + "\", \"format\": \"json-pretty\"}")
+    var c_buf *C.char = nil
+    var c_buf_len C.size_t = 0
+
+    ret := C.rados_mon_command(c.cluster, &c_cmd, 1, nil, 0, &c_buf, &c_buf_len, nil, nil)
+    defer C.free(unsafe.Pointer(c_buf))
+    if ret == 0 {
+        return C.GoStringN(c_buf, C.int(c_buf_len)), nil
+    } else {
+        return "", RadosError(ret)
+    }
+}
+
+func (c *Conn) GetECProfileName(poolname string) (string, error) {
+    c_cmd := C.CString("{\"prefix\":\"osd pool get\", \"var\": \"erasure_code_profile\", \"pool\": \"" + poolname + "\", \"format\": \"json-pretty\"}")
+    var c_buf *C.char = nil
+    var c_buf_len C.size_t = 0
+
+    ret := C.rados_mon_command(c.cluster, &c_cmd, 1, nil, 0, &c_buf, &c_buf_len, nil, nil)
+    defer C.free(unsafe.Pointer(c_buf))
+    if ret == 0 {
+        return C.GoStringN(c_buf, C.int(c_buf_len)), nil
+    } else {
+        return "", RadosError(ret)
+    }
+}
+
